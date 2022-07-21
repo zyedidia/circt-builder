339ca92faeeb2842bee81e61cc4516ea1b93b9bd
Zachary Yedidia <zyedidia@gmail.com>
2022-07-21 16:30:01 -0700

    Update tar source

--- tar-source.sh
+++ tar-source.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-REPO=https://github.com/llvm/circt
-CHECKOUT=sifive/1/10/0
-VERSION=1.10.0
+CHECKOUT=main
+VERSION=main
+# CHECKOUT=sifive/1/10/0
+# VERSION=1.10.0
 
-git clone $REPO circt-source
-cd circt-source
+cd circt
 git checkout $CHECKOUT
-git submodule init
-git submodule update
 git ls-files --recurse-submodules | tar caf ../circt-source-$VERSION.tar.gz -T- --xform s:^:circt/:
